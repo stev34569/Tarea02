1c0a9e53209bd8818b9681a276f841bbaa146936
steve <steve@Alonso>
2022-02-17 13:03:21 -0600

    Tarea02 sabados

---
+++ src/main/java/LugaresTuristicos/LugaresTuristicosApplication.java
@@ -0,0 +1,13 @@
+package LugaresTuristicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LugaresTuristicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LugaresTuristicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LugaresTuristicos/LugaresTuristicosApplicationTests.java
@@ -0,0 +1,13 @@
+package LugaresTuristicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LugaresTuristicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
